86e45ead169f8657a8df9d12b22a49d44adafe1d
Vladislav <trycatchernt@gmail.com>
2023-09-08 12:36:02 +0300

    added tests

--- src/polynomial_model.py
+++ src/polynomial_model.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-import data_gen
+from src import data_gen
 
 n = 20
 poly = 2
@@ -9,53 +9,53 @@ max_val = 10
 k = 0.001  # Increase k for higher degrees than 2
 steps = 1000
 
-a = 1 / (np.random.random(poly + 1) + 0.2 * (np.arange(poly + 1) + 1))
-a = a / (np.arange(poly + 1) + 1)
-a = a.reshape(-1, 1)
 
-
-def loss(x, y):
+def loss(x, y, params):
     """MSE loss function"""
-    result = x.dot(a) - y
+    result = x.dot(params) - y
     result = result ** 2
     return np.sum(result) / len(x)
 
 
-def gradient(x, y):
+def gradient(x, y, params):
     """
     Calculates gradient vector of loss function for X and Y
     :param x: matrix (n, poly + 1)
     :param y: matrix (n, 1)
+    :param params: model parameters
     :return: gradient vector with size n
     """
     # Vector of partial derivatives
-    df_da = np.zeros(poly + 1)
+    df_da = np.zeros(len(params))
     # For each partial derivative
-    for i in range(poly + 1):
+    for i in range(len(params)):
         # For each x
         for j in range(len(x)):
             value = 0
-            for p in range(poly + 1):
-                value += a[p] * x[j, p]
+            for p in range(len(params)):
+                value += params[p] * x[j, p]
             df_da[i] = df_da[i] + (value - y[j, 0]) * x[j, i]
     return df_da.reshape(-1, 1) / len(x)
 
 
 def main():
-    global a
+    a = 1 / (np.random.random(poly + 1) + 0.2 * (np.arange(poly + 1) + 1))
+    a = a / (np.arange(poly + 1) + 1)
+    a = a.reshape(-1, 1)
+
     x, y = data_gen.generate(n=n, poly=poly, max_val=max_val)
+    plt.plot(x[:, 1], y[:, 0], 'o')
 
     counter = 0
     a_array = []  # Array of several coefficients for plot
-    for i in range(steps):
+    for i in range(steps+1):
         a_array.append(a)
         if i % (steps/5) == 0:
-            print(f'{a.reshape(-1)}: {loss(x, y)}')
+            print(f'{a.reshape(-1)}: {loss(x, y, a)}')
             plt.plot(x[:, 1], np.sum(x.dot(a), axis=1), label=f'Step {i}')
             counter += 1
-        a = a - gradient(x, y) * k
+        a = a - gradient(x, y, a) * k
 
-    plt.plot(x[:, 1], y[:, 0], 'o')
     plt.title('Gradient Descent')
     plt.legend()
     plt.show()
---
+++ test/TestModel.py
@@ -0,0 +1,37 @@
+import unittest
+
+import numpy as np
+
+from src.polynomial_model import loss, gradient
+
+
+class TestModel(unittest.TestCase):
+    def test_loss(self):
+        a = np.array([0, 0]).reshape(-1, 1)
+        x = np.array([[1, 1],
+                      [1, 2]])
+        y = np.array([2, 3]).reshape(-1, 1)
+        self.assertEqual(loss(x, y, params=a), 6.5)
+
+    def test_loss_zero(self):
+        a = np.array([1, 1]).reshape(-1, 1)
+        x = np.array([[1, 1],
+                      [1, 2]])
+        y = np.array([2, 3]).reshape(-1, 1)
+        self.assertEqual(loss(x, y, params=a), 0)
+
+    def test_gradient(self):
+        a = np.array([0, 0]).reshape(-1, 1)
+        x = np.array([[1, 1],
+                      [1, 2]])
+        y = np.array([2, 3]).reshape(-1, 1)
+        # gradient function should return [[-2.5], [-4]], so, sum is 6.5
+        self.assertEqual(np.sum(gradient(x, y, params=a), axis=0), -6.5)
+
+    def test_gradient_zero(self):
+        a = np.array([1, 1]).reshape(-1, 1)
+        x = np.array([[1, 1],
+                      [1, 2]])
+        y = np.array([2, 3]).reshape(-1, 1)
+        # when loss=0, gradient is [[0], [0]]
+        self.assertEqual(np.sum(gradient(x, y, params=a), axis=0), 0)
